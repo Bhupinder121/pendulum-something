287379de8c1117c147937bd70255bfd8496fa0c1
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2023-01-18 19:53:03 +0530

    curves added

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 function setup(){
-
+    createCanvas(windowWidth, windowHeight);
+    
 }
 
 function draw(){
+    background(0);
+    stroke(0, 0, 255);
+    noFill();
+    for(let i = 0; i <= 400; i+=20){
+        halfCircle(i);
+    }
+    // noFill();
+    // bezier(0, height/2, width/2, 0, width/2, 0, width, height/2,)
+}
+
+function halfCircle(space){
+    beginShape();
+    curveVertex(0, height/2);
+    curveVertex(0, height/2);
+
     
+    curveVertex(width/2, height/2-space);
+
+
+    curveVertex(width, height/2);
+    curveVertex(width, height/2);
+    endShape();
 }
